227535ebea58dbb335f8d5a8b082230f3a58c086
Muhammad Uzair <34101864+uzair004@users.noreply.github.com>
2023-05-04 22:31:38 +0500

    Create createOrderUC.js

---
+++ src/use-cases/createOrderUC.js
@@ -0,0 +1,47 @@
+'use strict';
+
+function makeCreateOrderUC({ /*userDb*/ orderDb, makeOrder }) {
+  return async function createOrderUC(requestInfo) {
+    const { userId, products, remoteIp, apiInfo } = requestInfo;
+
+    for (const product of products) {
+      const { id, quantity } = product;
+      if (!id) {
+        return {
+          statusCode: 400,
+          body: { message: 'Invalid Product Requested' },
+        };
+      }
+
+      if (!quantity) {
+        return {
+          statusCode: 400,
+          body: { message: 'Invalid quantity Requested' },
+        };
+      }
+    }
+
+    // other business logic goes here
+
+    // e.g check if user exists, allowed to place order etc
+
+    // const user = await userDb.getUser({ userId });
+    // if (!user) {
+    //   return { statusCode: 404, body: { message: 'User Not Found!' } };
+    // }
+
+    // e.g verify if requested products exists and not out of stock
+
+    // create order
+    const order = makeOrder({
+      products,
+      userId,
+      remoteIp,
+      version: apiInfo.version,
+    });
+
+    await orderDb.createOrder(order.getItem());
+  };
+}
+
+module.exports = { makeCreateOrderUC };
