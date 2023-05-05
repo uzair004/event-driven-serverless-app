999566a2addcb9a38e12d244e68705a3aa1ffe8a
Muhammad Uzair <34101864+uzair004@users.noreply.github.com>
2023-05-05 22:20:03 +0500

    Update index.js

--- src/use-cases/index.js
+++ src/use-cases/index.js
@@ -6,14 +6,21 @@ const { orderDb } = require('../data-access');
 
 const { makeHelloWorldUC } = require('./helloWorldUC');
 const { makeCreateOrderUC } = require('./createOrderUC');
+const { makeProcessOrderUC } = require('./processOrderUC');
 
 const helloWorldUC = makeHelloWorldUC({});
 const createOrderUC = makeCreateOrderUC({ orderDb, makeOrder, pushToQueue });
+const processOrderUC = makeProcessOrderUC({ orderDb, makeOrder, pushToQueue });
 
-const requestService = Object.freeze({ helloWorldUC, createOrderUC });
+const requestService = Object.freeze({
+  helloWorldUC,
+  createOrderUC,
+  processOrderUC,
+});
 
 module.exports = {
   requestService,
   helloWorldUC,
   createOrderUC,
+  processOrderUC,
 };
