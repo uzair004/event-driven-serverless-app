e9b2f28f817df8ab86b9d138af92effa427b2152
Muhammad Uzair <34101864+uzair004@users.noreply.github.com>
2023-05-07 22:11:14 +0500

    Remove helloworld usecase

--- src/use-cases/index.js
+++ src/use-cases/index.js
@@ -4,20 +4,17 @@ const { pushToQueue } = require('../util/aws');
 const { makeOrder, makeProduct } = require('../domain/user');
 const { orderDb, productsDb } = require('../data-access');
 
-const { makeHelloWorldUC } = require('./helloWorldUC');
 const { makeCreateOrderUC } = require('./createOrderUC');
 const { makeProcessOrderUC } = require('./processOrderUC');
 const { makeGetCustomerOrdersUC } = require('./getCustomerOrdersUC');
 const { makeUpdateStockUC } = require('./updateStockUC');
 
-const helloWorldUC = makeHelloWorldUC({});
 const createOrderUC = makeCreateOrderUC({ orderDb, makeOrder, pushToQueue });
 const processOrderUC = makeProcessOrderUC({ orderDb, makeOrder, pushToQueue });
 const getCustomerOrdersUC = makeGetCustomerOrdersUC({ orderDb, makeOrder });
 const updateStockUC = makeUpdateStockUC({ productsDb, makeProduct });
 
 const requestService = Object.freeze({
-  helloWorldUC,
   createOrderUC,
   processOrderUC,
   getCustomerOrdersUC,
@@ -26,7 +23,6 @@ const requestService = Object.freeze({
 
 module.exports = {
   requestService,
-  helloWorldUC,
   createOrderUC,
   processOrderUC,
   getCustomerOrdersUC,
