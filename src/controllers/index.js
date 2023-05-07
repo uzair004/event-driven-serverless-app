1affa390059d95984e6c0be8307100f18b022c13
Muhammad Uzair <34101864+uzair004@users.noreply.github.com>
2023-05-07 22:10:18 +0500

    Remove sample helloWorld function

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,26 +1,16 @@
 'use strict';
 
-const { getApiInfo, isValidEmail, missingItem } = require('../util/util');
+const { getApiInfo, missingItem } = require('../util/util');
 
 const {
-  helloWorldUC,
   createOrderUC,
   processOrderUC,
-  getCustomerOrdersUC,
   updateStockUC,
 } = require('../use-cases');
-const { makeHelloWorldC } = require('./helloWorldC');
 const { makeCreateOrderC } = require('./createOrderC');
 const { makeProcessOrderC } = require('./processOrderC');
 const { makeGetCustomerOrdersC } = require('./getCustomerOrdersC');
 const { makeUpdateStockC } = require('./updateStockC');
-
-const helloWorldC = makeHelloWorldC({
-  helloWorldUC,
-  getApiInfo,
-  isValidEmail,
-  missingItem,
-});
 
 const createOrderC = makeCreateOrderC({
   getApiInfo,
@@ -33,13 +23,11 @@ const processOrderC = makeProcessOrderC({ processOrderUC });
 const getCustomerOrdersC = makeGetCustomerOrdersC({
   getApiInfo,
   missingItem,
-  getCustomerOrdersUC,
 });
 
 const updateStockC = makeUpdateStockC({ updateStockUC });
 
 const requestController = Object.freeze({
-  helloWorldC,
   createOrderC,
   processOrderC,
   getCustomerOrdersC,
@@ -47,7 +35,6 @@ const requestController = Object.freeze({
 });
 
 module.exports = {
-  helloWorldC,
   createOrderC,
   processOrderC,
   requestController,
